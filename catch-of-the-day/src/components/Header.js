8bad6b7e04cf7e15eb24a31a8dc9d386b7d9e503
Jaytrin <jaytrin.apinchapong@gmail.com>
2019-03-13 23:44:10 -0700

    added router to app

--- catch-of-the-day/src/components/Header.js
+++ catch-of-the-day/src/components/Header.js
@@ -1,8 +1,6 @@
 import React from 'react';
 
-class Header extends React.Component {
-    render () {
-        return (
+const Header = (props) => (
             <>
             <header className="top">
                 <h1>
@@ -15,11 +13,33 @@ class Header extends React.Component {
                     </h1>
             </header>
             <h3 className="tagline">
-                <span>Fresh Daily</span>
+                <span>{props.tagline}</span>
             </h3>
             </>
-        )
-    }
-}
+    );
+
+
+
+// class Header extends React.Component {
+//     render () {
+//         return (
+//             <>
+//             <header className="top">
+//                 <h1>
+//                     Catch 
+//                     <span className="ofThe">
+//                         <span className="of">Of</span>
+//                         <span className="the">The</span>
+//                     </span>
+//                     Day
+//                     </h1>
+//             </header>
+//             <h3 className="tagline">
+//                 <span>{this.props.tagline}</span>
+//             </h3>
+//             </>
+//         )
+//     }
+// }
 
 export default Header;
